fa2521c467b2fc11fbba58bcef127d57083acda3
PC1 <kevsalomdev@gmail.com>
2024-10-02 10:30:09 -0400

    set github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://kevsalom.github.io",
+  base: "prueba-github-pages",
+});
